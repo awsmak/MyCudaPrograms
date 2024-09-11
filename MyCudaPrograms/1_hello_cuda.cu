3612b6d4a1319e90c8536cc1587fb7c2dd69a9d1
awsmak <akash.47.de@gmail.com>
2024-09-11 14:56:37 +0200

    do programmin assignment 1

---
+++ MyCudaPrograms/1_hello_cuda.cu
@@ -0,0 +1,21 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+//#include <stdio.h>
+//
+//static __global__ void hello_kernel()
+//{
+//	printf("Hello cuda world \n");
+//}
+//
+//int main()
+//{
+//	printf("hello from main \n");
+//
+//	// Launch kernel with 1 block and 20 threads
+//	hello_kernel <<<1, 20>> > ();
+//
+//	cudaDeviceSynchronize();
+//	cudaDeviceReset();
+//	return 0;
+//}
---
+++ MyCudaPrograms/3_organization_of_threads.cu
@@ -0,0 +1,33 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+//__global__ void print_threadIds()
+//{
+//	printf("threadIdx.x : %d, threadIdx.y : %d, threadIdx.z : %d \n",
+//		threadIdx.x, threadIdx.y, threadIdx.z);
+//}
+//
+//__global__ void print_details()
+//{
+//	printf("blockIdx.x : %d, blockIdx.y : %d, blockIdx.z : %d, blockDim.x : %d, blockDim.y : %d, gridDim.x : %d, gridDim.y :%d \n",
+//		blockIdx.x, blockIdx.y, blockIdx.z, blockDim.x, blockDim.y, gridDim.x, gridDim.y);
+//}
+
+//int main()
+//{
+//	int nx, ny;
+//	nx = 16;
+//	ny = 16;
+//
+//	dim3 block(8, 8);
+//	dim3 grid(nx / block.x, ny / block.y);
+//
+//	//print_threadIds << <grid, block >> > ();
+//	print_details <<<grid, block >>> ();
+//	cudaDeviceSynchronize();
+//
+//	cudaDeviceReset();
+//	return 0;
+//}
